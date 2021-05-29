a3e0b6e21d8c16e2e537c1c0f96a504f74a67d88
Gregory Brutsche <gregory@mail.brutsches.local>
2021-05-29 14:52:55 -0400

    Initial commit.

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using Base_Mod;
+using JetBrains.Annotations;
+
+namespace XMod {
+    [UsedImplicitly]
+    public class Plugin : BaseGameMod {
+        protected override string ModName => "Recipe-Show-Have-Count";
+
+        protected override void Init() {
+            Recipe_Available_Input_Count.Plugin.showAmountOfCurrentItem = true;
+
+            base.Init();
+        }
+    }
+}
